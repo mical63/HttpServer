cfa8c50e84f056f27f14cbc7badfdeb0ab897e26
mical63 <15932656692@163.com>
2025-06-10 15:23:01 +0800

    FIGHTgit add .!

---
+++ build/CMakeFiles/a.out.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mical/HttpSevver/HttpServer-C/src/Buffer.c" "CMakeFiles/a.out.dir/src/Buffer.c.o" "gcc" "CMakeFiles/a.out.dir/src/Buffer.c.o.d"
+  "/home/mical/HttpSevver/HttpServer-C/src/ChannelMap.c" "CMakeFiles/a.out.dir/src/ChannelMap.c.o" "gcc" "CMakeFiles/a.out.dir/src/ChannelMap.c.o.d"
+  "/home/mical/HttpSevver/HttpServer-C/src/EpollDispatcher.c" "CMakeFiles/a.out.dir/src/EpollDispatcher.c.o" "gcc" "CMakeFiles/a.out.dir/src/EpollDispatcher.c.o.d"
+  "/home/mical/HttpSevver/HttpServer-C/src/EventLoop.c" "CMakeFiles/a.out.dir/src/EventLoop.c.o" "gcc" "CMakeFiles/a.out.dir/src/EventLoop.c.o.d"
+  "/home/mical/HttpSevver/HttpServer-C/src/HttpRequest.c" "CMakeFiles/a.out.dir/src/HttpRequest.c.o" "gcc" "CMakeFiles/a.out.dir/src/HttpRequest.c.o.d"
+  "/home/mical/HttpSevver/HttpServer-C/src/HttpResponse.c" "CMakeFiles/a.out.dir/src/HttpResponse.c.o" "gcc" "CMakeFiles/a.out.dir/src/HttpResponse.c.o.d"
+  "/home/mical/HttpSevver/HttpServer-C/src/PollDispatcher.c" "CMakeFiles/a.out.dir/src/PollDispatcher.c.o" "gcc" "CMakeFiles/a.out.dir/src/PollDispatcher.c.o.d"
+  "/home/mical/HttpSevver/HttpServer-C/src/SelectDispatcher.c" "CMakeFiles/a.out.dir/src/SelectDispatcher.c.o" "gcc" "CMakeFiles/a.out.dir/src/SelectDispatcher.c.o.d"
+  "/home/mical/HttpSevver/HttpServer-C/src/TcpConnection.c" "CMakeFiles/a.out.dir/src/TcpConnection.c.o" "gcc" "CMakeFiles/a.out.dir/src/TcpConnection.c.o.d"
+  "/home/mical/HttpSevver/HttpServer-C/src/TcpServer.c" "CMakeFiles/a.out.dir/src/TcpServer.c.o" "gcc" "CMakeFiles/a.out.dir/src/TcpServer.c.o.d"
+  "/home/mical/HttpSevver/HttpServer-C/src/ThreadPool.c" "CMakeFiles/a.out.dir/src/ThreadPool.c.o" "gcc" "CMakeFiles/a.out.dir/src/ThreadPool.c.o.d"
+  "/home/mical/HttpSevver/HttpServer-C/src/WorkerThread.c" "CMakeFiles/a.out.dir/src/WorkerThread.c.o" "gcc" "CMakeFiles/a.out.dir/src/WorkerThread.c.o.d"
+  "/home/mical/HttpSevver/HttpServer-C/src/channel.c" "CMakeFiles/a.out.dir/src/channel.c.o" "gcc" "CMakeFiles/a.out.dir/src/channel.c.o.d"
+  "/home/mical/HttpSevver/HttpServer-C/src/main.c" "CMakeFiles/a.out.dir/src/main.c.o" "gcc" "CMakeFiles/a.out.dir/src/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/a.out.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a.out.dir/src/Buffer.c.o"
+  "CMakeFiles/a.out.dir/src/Buffer.c.o.d"
+  "CMakeFiles/a.out.dir/src/ChannelMap.c.o"
+  "CMakeFiles/a.out.dir/src/ChannelMap.c.o.d"
+  "CMakeFiles/a.out.dir/src/EpollDispatcher.c.o"
+  "CMakeFiles/a.out.dir/src/EpollDispatcher.c.o.d"
+  "CMakeFiles/a.out.dir/src/EventLoop.c.o"
+  "CMakeFiles/a.out.dir/src/EventLoop.c.o.d"
+  "CMakeFiles/a.out.dir/src/HttpRequest.c.o"
+  "CMakeFiles/a.out.dir/src/HttpRequest.c.o.d"
+  "CMakeFiles/a.out.dir/src/HttpResponse.c.o"
+  "CMakeFiles/a.out.dir/src/HttpResponse.c.o.d"
+  "CMakeFiles/a.out.dir/src/PollDispatcher.c.o"
+  "CMakeFiles/a.out.dir/src/PollDispatcher.c.o.d"
+  "CMakeFiles/a.out.dir/src/SelectDispatcher.c.o"
+  "CMakeFiles/a.out.dir/src/SelectDispatcher.c.o.d"
+  "CMakeFiles/a.out.dir/src/TcpConnection.c.o"
+  "CMakeFiles/a.out.dir/src/TcpConnection.c.o.d"
+  "CMakeFiles/a.out.dir/src/TcpServer.c.o"
+  "CMakeFiles/a.out.dir/src/TcpServer.c.o.d"
+  "CMakeFiles/a.out.dir/src/ThreadPool.c.o"
+  "CMakeFiles/a.out.dir/src/ThreadPool.c.o.d"
+  "CMakeFiles/a.out.dir/src/WorkerThread.c.o"
+  "CMakeFiles/a.out.dir/src/WorkerThread.c.o.d"
+  "CMakeFiles/a.out.dir/src/channel.c.o"
+  "CMakeFiles/a.out.dir/src/channel.c.o.d"
+  "CMakeFiles/a.out.dir/src/main.c.o"
+  "CMakeFiles/a.out.dir/src/main.c.o.d"
+  "a.out"
+  "a.out.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/a.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
